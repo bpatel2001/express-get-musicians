e7d791ee04db7004a8fd56fb97f4112664b842c6
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-19 11:06:35 -0500

    Finished workshop and the extensions

--- index.test.js
+++ index.test.js
@@ -5,18 +5,52 @@ execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician, Band } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
+    test('GET /musicians request is successful', async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.statusCode).toBe(200);
+    });
 
+    test('GET /musicians returns all musicians', async () => {
+        const musicians = await Musician.findAll();
+        const response = await request(app).get('/musicians');
+        const responseData = JSON.parse(response.text);
 
+        const musiciansJSON = musicians.map(musician => {
+            const musicianJSON = musician.toJSON();
+            musicianJSON.createdAt = musicianJSON.createdAt.toISOString();
+            musicianJSON.updatedAt = musicianJSON.updatedAt.toISOString();
+            return musicianJSON;
+        });
 
+        expect(responseData).toEqual(musiciansJSON);
+    });
+})
 
-    
+describe('./bands endpoint', () => {
+    test('GET /bands request is successful', async () => {
+        const response = await request(app).get('/bands');
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('GET /bands returns all bands', async () => {
+        const bands = await Band.findAll();
+        const response = await request(app).get('/bands');
+        const responseData = JSON.parse(response.text);
+
+        const bandsJSON = bands.map(band => {
+            const bandJSON = band.toJSON();
+            bandJSON.createdAt = bandJSON.createdAt.toISOString();
+            bandJSON.updatedAt = bandJSON.updatedAt.toISOString();
+            return bandJSON;
+        });
+
+        expect(responseData).toEqual(bandsJSON);
+    });
 })
--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (request, response) => {
+    const musicians = await Musician.findAll();
+    response.json(musicians);
+})
 
+app.get("/bands", async (request, response) => {
+    const bands = await Band.findAll();
+    response.json(bands);
+})
 
-
-
+app.get("/musicians/:id", async (request, response) => {
+    const musician = await Musician.findByPk(request.params.id);
+    response.json(musician);
+})
 
 
 module.exports = app;
