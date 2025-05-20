c02a8c9304cad8b5f3b2c302d8166e36b6d9a44d
bpatel2001 <bpatel2001h@gmail.com>
2025-05-20 12:16:30 -0500

    finished part 2 and 3

--- index.test.js
+++ index.test.js
@@ -31,6 +31,22 @@ describe('./musicians endpoint', () => {
 
         expect(responseData).toEqual(musiciansJSON);
     });
+
+    test('POST /musicians request is successful', async () => {
+        const response = await request(app).post('/musicians').send(seedMusician[0]);
+        expect(response.statusCode).toBe(200);
+    });
+
+    test ('POST /musicians returns an error', async () => {
+        const response = await request(app).post('/musicians');
+        expect(response.statusCode).toBe(400);
+    })
+
+    test('POST /musicians creates a new musician', async () => {
+        const response = await request(app).post('/musicians').send(seedMusician[0]);
+        const musician = await Musician.findByPk(response.body.id);
+        expect(musician).toBeTruthy();
+    });
 })
 
 describe('./bands endpoint', () => {
@@ -61,8 +77,40 @@ describe('./musicians/:id endpoint', () => {
         expect(response.statusCode).toBe(200);
     });
 
-    test('GET /musicians/4 returns an error', async () => {
-        const response = await request(app).get('/musicians/4');
+    test('GET /musicians/:id returns an error', async () => {
+        const response = await request(app).get('/musicians/10');
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('PUT /musicians/:id request is successful', async () => {
+        const response = await request(app).put('/musicians/1').send({ name: 'John Doe' });
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('PUT /musicians/:id updates a musician', async () => {
+        const response = await request(app).put('/musicians/1').send({ name: 'John Doe' });
+        const musician = await Musician.findByPk(1);
+        expect(musician.name).toBe('John Doe');
+    });
+
+    test('PUT /musicians/:id returns an error', async () => {
+        const response = await request(app).put('/musicians/10').send({ name: 'John Doe' });
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('DELETE /musicians/:id request is successful', async () => {
+        const response = await request(app).delete('/musicians/1');
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('DELETE /musicians/:id deletes a musician', async () => {
+        const response = await request(app).delete('/musicians/1');
+        const musician = await Musician.findByPk(1);
+        expect(musician).toBeFalsy();
+    });
+
+    test('DELETE /musicians/:id returns an error', async () => {
+        const response = await request(app).delete('/musicians/10');
         expect(response.statusCode).toBe(404);
     });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create a GET /musicians route to return all musicians 
 
 app.get("/musicians", async (request, response) => {
@@ -25,5 +28,37 @@ app.get("/musicians/:id", async (request, response) => {
     response.json(musician);
 })
 
+app.post("/musicians", async (request, response) => {
+    if(!request.body || Object.keys(request.body).length === 0) {
+        return response.status(400).json({ error: "Invalid request body" });
+    }
+
+    const musician = await Musician.create(request.body);
+
+    console.log(request.body);
+    response.json(musician);
+})
+
+app.put("/musicians/:id", async (request, response) => {
+    const musicianId = request.params.id;
+    const musician = await Musician.update(request.body, {
+        where: { id: musicianId }
+    });
+    if (!musician || musician[0] === 0) {
+        return response.status(404).json({ error: "Musician not found" });
+    }
+    response.json(musician);
+})
+
+app.delete("/musicians/:id", async (request, response) => {
+    const musicianId = request.params.id;
+    const musician = await Musician.destroy({
+        where: { id: musicianId }
+    });
+    if (!musician) {
+        return response.status(404).json({ error: "Musician not found" });
+    }
+    response.json(musician);
+})
 
 module.exports = app;
